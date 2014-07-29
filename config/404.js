2bcacb432e26bd832a187c8c81d802c8df9dd4c7
Tomas Kislan <kislan.tomas@gmail.com>
2014-07-29 22:41:59 +0200

    Added auto redirect on 404

--- config/404.js
+++ config/404.js
@@ -2,7 +2,7 @@
  * Default 404 (Not Found) handler
  *
  * If no route matches are found for a request, Sails will respond using this handler.
- * 
+ *
  * This middleware can also be invoked manually from a controller or policy:
  * Usage: res.notFound()
  */
@@ -15,24 +15,26 @@ module.exports[404] = function pageNotFound(req, res) {
    * the same interface for receiving socket messages.
    */
 
-  var viewFilePath = '404';
-  var statusCode = 404;
-  var result = {
-    status: statusCode
-  };
+  res.redirect('/');
 
-  // If the user-agent wants a JSON response, send json
-  if (req.wantsJSON) {
-    return res.json(result, result.status);
-  }
+  // var viewFilePath = '404';
+  // var statusCode = 404;
+  // var result = {
+  //   status: statusCode
+  // };
 
-  res.status(result.status);
-  res.render(viewFilePath, function (err) {
-    // If the view doesn't exist, or an error occured, send json
-    if (err) { return res.json(result, result.status); }
+  // // If the user-agent wants a JSON response, send json
+  // if (req.wantsJSON) {
+  //   return res.json(result, result.status);
+  // }
 
-    // Otherwise, serve the `views/404.*` page
-    res.render(viewFilePath);
-  });
+  // res.status(result.status);
+  // res.render(viewFilePath, function (err) {
+  //   // If the view doesn't exist, or an error occured, send json
+  //   if (err) { return res.json(result, result.status); }
+
+  //   // Otherwise, serve the `views/404.*` page
+  //   res.render(viewFilePath);
+  // });
 
 };
